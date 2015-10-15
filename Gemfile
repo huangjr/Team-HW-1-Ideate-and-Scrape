663ea2d2790aae52b0ed3fcf8d729009c8c49a96
pengyuchen <pengyu@nlplab.cc>
2015-10-15 12:59:17 +0800

    vcr test

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'nokogiri'
 
 group  :test do
     gem 'rake'
+    gem 'vcr'
+    gem 'webmock'
     gem 'minitest'
     gem 'minitest-rg'
 end
--- spec/code_school_spec.rb
+++ spec/code_school_spec.rb
@@ -1,17 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/rg'
+require 'vcr'
+require 'webmock/minitest'
 require_relative '../lib/scrape'
 
-describe 'test for code school' do
-  before do
-    cs = SiteScraper.new
-    @codeschool = cs.code_school_data
-    @output = File.read('./spec/testfiles/output.json')
-  end
-  it 'has same length' do
-    @codeschool.size.must_equal @output.chomp.length
-  end
-  it 'should be same' do
-    JSON.parse(@codeschool).must_equal JSON.parse(@output)
+VCR.configure do |config|
+    config.cassette_library_dir = 'fixtures/vcr/cassettes'
+    config.hook_into :webmock
+end
+
+VCR.use_cassette('codeschool') do
+  
+  codeschool = SiteScraper.new.code_school_data
+  golden     = File.read('fixtures/output.json')
+  
+  describe 'test for code school' do
+    it 'has same length' do
+      codeschool.size.must_equal output.chomp.length
+    end
+    it 'should be same' do
+      JSON.parse(codeschool).must_equal JSON.parse(output)
+    end
   end
 end
